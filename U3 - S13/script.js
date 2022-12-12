dbba06afdafa59aaac67f317c0d49003e533cbc1
erikson <erikson.velro@gmail.com>
2022-12-12 16:33:30 -0500

    Ejercicio con Angular

---
+++ U3 - S13/script.js
@@ -0,0 +1,20 @@
+// datos
+//  if <= 300km = 250
+//  if >300km & <= 1000km = 250 + 30 * cada kilometro
+//  if > 1000 = 250 + (30 * cada kilometro recorrido despues de 300km) + (20 * cada kilometro recorrido despues de 1000)
+//  21000 soles ( 300km hasta 1000km)
+//  descuento de 10% si se paga más de 500 soles
+
+let app = angular.module('myApp', []);
+app.controller('myCtrl', function ($scope) {
+  $scope.distance = 0;
+  $scope.calcularTarifa = function () {
+    if ($scope.distance <= 300) {
+      return 250;
+    } else if ($scope.distance <= 1000) {
+      return 250 + 30 * ($scope.distance - 300);
+    } else {
+      return 250 + 30 * (1000 - 300) + 20 * ($scope.distance - 1000);
+    }
+  };
+});
